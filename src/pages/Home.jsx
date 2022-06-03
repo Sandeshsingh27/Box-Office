1da9f64b0f9428692e41f88ce031b075254e7a6c
Sandesh Singh <sandeshsingh295@gmail.com>
2022-06-03 22:30:00 +0530

    created cards for api data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
@@ -16,30 +18,31 @@ const Home = () => {
   };
 
   const onInputChange = ev => {
-    setInput(ev.target.value);
+    setInput(ev.target.value); // capture the inputted value in the search box
   };
 
   const onRadioChange = ev => {
-    setSearchOptions(ev.target.value);
+    setSearchOptions(ev.target.value); // to correct some anonymous behaviour of radio button
   };
 
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
-      onSearch();
+      onSearch(); // so that we can search using enter button
     }
   };
 
+  // displays the result from the api
   const renderResult = () => {
     if (results && results.length === 0) {
       return <div>No Results</div>;
     }
 
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
